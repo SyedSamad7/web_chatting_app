542d62b1ab11c37144e61223356f0d74b0914798
SyedSamad7 <syedsamadshah15@gmail.com>
2024-10-17 06:06:05 -0700

    delete future builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:web_chat_app/firebase_options.dart';
+import 'package:web_chat_app/future_builder.dart';
 import 'costom_painter.dart';
 
 void main() async {
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const CostomPainter(),
+      home: const FutureBuilderWidget(),
     );
   }
 }
